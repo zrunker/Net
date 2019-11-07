694b8f030281e1a36ce42a6cf2cd950ec1d2d274
zrunker <zrunker@yahoo.com>
2019-11-07 10:21:49 +0800

    init Commit

---
+++ netlib/src/main/java/cc/ibooker/netlib/listeners/OnModelListener.java
@@ -0,0 +1,16 @@
+package cc.ibooker.netlib.listeners;
+
+import cc.ibooker.netlib.dto.ErrorData;
+
+// 网络请求接口
+public interface OnModelListener<T> {
+    void onStart();
+
+    void onError(ErrorData errorData);
+
+    void onLogin(ErrorData errorData);
+
+    void onCompleted();
+
+    void onNext(T data);
+}
